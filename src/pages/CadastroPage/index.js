1fd3ee8e13cf6a6ad06e222383267ccc956d854a
Alexandre <154011333+xandde@users.noreply.github.com>
2025-06-06 17:08:10 -0300

    Add files via upload

---
+++ src/pages/CadastroPage/index.js
@@ -0,0 +1,11 @@
+import CadastroPrato from '../../components/CadastroPrato'
+import './styles.css'
+
+export default function CadastroPage() {
+  return (
+    <div className="cadastro-page">
+      <h1>Cadastro de Prato</h1>
+      <CadastroPrato />
+    </div>
+  )
+}
---
+++ src/pages/CardapioPage/index.js
@@ -0,0 +1,11 @@
+import Cardapio from '../../components/Cardapio'
+import './styles.css'
+
+export default function CardapioPage() {
+  return (
+    <div className="cardapio-page">
+      <h1>Cardápio</h1>
+      <Cardapio />
+    </div>
+  )
+}
